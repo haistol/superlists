dde1b0742be93571542afda8572b6b6b1940a771
Juan Ramon De la Rosa <j.delarosa48@gmail.com>
2017-02-05 22:59:06 -0400

    fix fab excecution error when website is runnig

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -7,12 +7,14 @@ REPO_URL = 'https://github.com/haistol/superlists.git'
 def deploy():
         site_folder = '/home/%s/sites/%s' % (env.user, env.host)
         source_folder = site_folder + '/source'
+        run('sudo systemctl stop gunicorn-%s' % (env.host,))
         _create_directory_structure_if_necessary(site_folder)
         _get_latest_source(source_folder)
         _update_settings(source_folder, env.host)
         _update_virtualenv(source_folder)
         _update_static_files(source_folder)
         _update_database(source_folder)
+        run('sudo systemctl start gunicorn-%s' % (env.host,))
 
 def _create_directory_structure_if_necessary(site_folder):
         for subfolder in ('database', 'static', 'virtualenv', 'source'):
