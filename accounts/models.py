d00e14ab21d61bc0ac5763ad37688091429f1749
haistol <darkwizard48@gmail.com>
2017-02-08 20:07:44 +0000

    User and Token models coded

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class User(models.Model):
+    email = models.EmailField(primary_key=True)
+    REQUIRED_FIELDS = ()
+    USERNAME_FIELD = 'email'
+    
+    def is_authenticated(self):
+        return True
+        
+class Token(models.Model):
+    email = models.EmailField()
+    uid = models.CharField(default= uuid.uuid4, max_length=40)
