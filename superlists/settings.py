6d836adad4ccbc6090d6b8351f5befc80205eb01
Juan Ramon De la Rosa <j.delarosa48@gmail.com>
2017-02-07 21:18:29 -0400

    spiked in custom passwordless auth backend

--- superlists/settings.py
+++ superlists/settings.py
@@ -109,7 +109,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR,'../static'))
 
-EMAIL_HOST = 'smpt.dw48studio.tk'
+
+
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 EMAIL_USE_TLS = True
